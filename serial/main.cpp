d3d77c25e2d5eb85b4f0bbf1fd5771e6dc7ca511
ariangh001 <ariangh001@gmail.com>
2020-12-11 18:03:03 +0330

    Implemented several functions in order to create a Dataset with proper types from CSV Files (No Bugs)!!

--- serial/main.cpp
+++ serial/main.cpp
@@ -17,7 +17,8 @@ using namespace std;
 typedef std::string Record;
 typedef std::vector<std::string> Records;
 typedef std::vector<std::string> Words;
-
+typedef std::vector<float> Features;
+typedef std::vector<std::vector<float>> Dataset;
 
 int checkInput(int argc)
 {
@@ -27,6 +28,26 @@ int checkInput(int argc)
         return -1;
     }
     return 0;
+}
+
+Words parseString(string str, char delimiter)
+{
+    Words words;
+    string tmp = "";
+    for (auto x : str)
+    {
+        if(x == delimiter)
+        {
+            words.push_back(tmp);
+            tmp = "";
+        }
+        else
+        {
+            tmp += x;
+        }
+    }
+    words.push_back(tmp);
+    return words;
 }
 
 Records readCSV(string filename)
@@ -48,9 +69,40 @@ Records readCSV(string filename)
     return records;
 }
 
+Features changeType(Record record)
+{
+    Features features;
+    Words words = parseString(record,COMMA);
+    for (int j=0; j<words.size(); j++)
+        features.push_back(stof(words[j]));
+    return features;
+}
+
+Dataset createDataset(Records records)
+{
+    Dataset dataset;
+    for (int i=0; i<records.size(); i++)
+        dataset.push_back(changeType(records[i]));
+    return dataset;
+}
+
+
 int main(int argc , char* argv[])
 {
     if(checkInput(argc) < 0)
         return -1;
+
+    string main_dir(argv[1]);
+    string dataset_dir = main_dir;
+    dataset_dir.append(DATASET_FILE);
+    string weights_dir = main_dir;
+    weights_dir.append(WEIGHT_FILE);
+
+    Records phones = readCSV(dataset_dir);
+    Records weights = readCSV(weights_dir);
+
+    Dataset phones_dataset = createDataset(phones);
+    Dataset weights_dataset = createDataset(weights);
+
     return 0;
 }
